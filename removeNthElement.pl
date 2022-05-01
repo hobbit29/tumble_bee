48a325a70939eadeef74df4976a1c5a859552fa3
hobbit29 <104728293+hobbit29@users.noreply.github.com>
2022-05-01 22:22:50 +0530

    Update removeNthElement.pl

--- removeNthElement.pl
+++ removeNthElement.pl
@@ -1,2 +1,4 @@
 removen([_|List],1,List).
 removen([H|List],Pos,[H|Result]):-Pos1 is Pos-1, removen(List,Pos1,Result).
+
+% output?- removen([List], x , newList). to remove x from the [List to get newList]
