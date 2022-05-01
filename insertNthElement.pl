9195f34ca88a75f0c1ffbe7719587443685cb776
hobbit29 <104728293+hobbit29@users.noreply.github.com>
2022-05-01 22:21:30 +0530

    Update insertNthElement.pl

--- insertNthElement.pl
+++ insertNthElement.pl
@@ -1,2 +1,4 @@
 insertn(Item,List,1,[Item|List]).
 insertn(Item,[H|List],Pos,[H|Result]):-Pos1 is Pos-1,insertn(Item,List,Pos1,Result).
+
+% output?- insertn(x, [list], position, newList). to insert x at 'position' to get a newList
