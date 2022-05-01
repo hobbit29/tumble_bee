d0fb169756861bab2f5bd4e159f63e705b6fd1ca
hobbit29 <104728293+hobbit29@users.noreply.github.com>
2022-05-01 22:27:01 +0530

    Update merge2List.pl

--- merge2List.pl
+++ merge2List.pl
@@ -3,3 +3,5 @@ merge([],Y,Y).
 merge([X|X1],[Y|Y1],[X|Z]):-X<Y,!,merge(X1,[Y|Y1],Z).
 merge([X|X1],[Y|Y1],[X,Y|Z]):-X=Y,!,merge(X1,Y1,Z).
 merge([X|X1],[Y|Y1],[Y|Z]):-X>Y,!,merge([X|X1],Y1,Z).
+
+% output?- merge([1,3,5], [2,4,6], L). to merge list in sorted manner
